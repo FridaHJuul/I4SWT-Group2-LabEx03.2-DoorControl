10143a1a1857c124ef362595db1e8baf431f1365
megsgaard <mathias.egsgaard@post.au.dk>
2019-02-13 11:53:36 +0100

    Tilføjet implementering i DoorControl

--- DoorControlProject/DoorControl.cs
+++ DoorControlProject/DoorControl.cs
@@ -8,7 +8,33 @@ namespace DoorControlProject
 {
    public class DoorControl
    {
+      private IUserValidation _userValidation;
+      private IAlarm _alarm;
+      private IDoor _door;
+      private IEntryNotification _entryNotification;
+
+      public DoorControl(IUserValidation userValidation, IAlarm alarm, IDoor door, IEntryNotification entryNotification)
+      {
+         _userValidation = userValidation;
+         _alarm = alarm;
+         _door = door;
+         _entryNotification = entryNotification;
+      }
+
       public void RequestEntry(string id)
-      { }
+      {
+         var idStatus = _userValidation.ValidateEntryRequest(id);
+
+         if (idStatus)
+         {
+            _door.Open();
+            _entryNotification.NotifyEntryGranted();
+         }
+      }
+
+      public void DoorOpen()
+      {
+         _door.Open();
+      }
    }
 }

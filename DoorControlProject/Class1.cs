10d32d098490b926b7a98007fb9238814e8b2d3b
FridaHJuul <frida.hvelplund.juul@post.au.dk>
2019-02-13 11:01:02 +0100

    Add project files.

---
+++ DoorControlProject/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControlProject
+{
+    public class Class1
+    {
+    }
+}
